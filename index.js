46ae6c70580d91dd44ff720fd7ca8ed2935c3010
Alex Patterson <alex@codingcat.dev>
2024-06-21 13:30:31 -0400

    changes

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ const proxy = require("express-http-proxy");
 const app = require("express")();
 
 const port = process.env.PORT || 3001;
-const fusionAuthURL =
-  process.env.FUSIONAUTH_URL || "fusionauth-app-latest:9011";
+const targetServerURL = process.env.TARGET_SERVER_URL || "localhost:9011"; // Use TARGET_SERVER_URL for flexibility
 
-app.use(
+// Proxy all requests to the target server
+
+app.all(
   "*",
-  proxy(`http://${fusionAuthURL}`, (req, res, next) => {
-    req.headers["X-Forwarded-Port"] = 443;
-    next();
+  proxy(`http://${targetServerURL}`, {
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      // You can add custom middleware here if needed
+      proxyReqOpts.headers["X-Forwarded-Port"] = port;
+      return proxyReqOpts;
+    },
   })
 );
 
 const server = app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`Proxy server listening on port ${port}!`)
 );
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
