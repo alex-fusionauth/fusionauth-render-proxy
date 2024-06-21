16d9e29ad70e9495848b608d59beb19919a79067
Alex Patterson <alex@codingcat.dev>
2024-06-21 13:42:03 -0400

    update for render items

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ const app = require("express")();
 const port = process.env.PORT || 3001;
 const targetServerURL =
   process.env.RENDER_FUSIONAUTH_INTERNAL_URL || "localhost:9011";
-// Proxy all requests to the target server
 
+const renderHostname = process.env.RENDER_EXTERNAL_HOSTNAME;
+
+// Proxy all requests to the fusionauth server
 app.all(
   "*",
   proxy(`http://${targetServerURL}`, {
     proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
       // You can add custom middleware here if needed
-      proxyReqOpts.headers["X-Forwarded-Port"] = port;
+      if (renderHostname) {
+        proxyReqOpts.headers["X-Forwarded-Host"] = renderHostname;
+        proxyReqOpts.headers["X-Forwarded-Port"] = 443;
+      } else {
+        proxyReqOpts.headers["X-Forwarded-Port"] = port;
+      }
       return proxyReqOpts;
     },
   })
